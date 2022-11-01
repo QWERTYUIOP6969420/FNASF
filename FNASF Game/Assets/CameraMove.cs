affa5dc759a2bbb77acdbd2bda9764208aca77f6
QWERTYUIOP6969420 <qwertyuiop69694201337@gmail.com>
2022-11-01 18:47:34 -0400

    Camera Movement Draft

--- FNASF Game/Assets/CameraMove.cs
+++ FNASF Game/Assets/CameraMove.cs
@@ -4,18 +4,29 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
-    public float turn_speed = 200f;
+    public float turn_speed = 50f;
 
     public Transform player_body;
 
     public float screen_size = Screen.width;
-    public float left_screen_size = screen_size / 3;
-    public float right_screen_size = left_screen_size + right_screen_size / 3;
+    public float left_screen_size;
+    public float right_screen_size;
+
+    void Start() {
+        left_screen_size = screen_size / 3;
+        right_screen_size = left_screen_size * 2 + screen_size / 3;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        float mouse_x = Input.GetAxis("Mouse X") * turn_speed * Time.deltaTime;
-        player_body.Rotate(Vector3.up * mouse_x);
+        Vector3 mouse_pos = Input.mousePosition;
+        if(mouse_pos.x <= left_screen_size){
+            player_body.Rotate(Vector3.up * (-Time.deltaTime * turn_speed));
+        } else if(mouse_pos.x >= right_screen_size){
+            player_body.Rotate(Vector3.up * (Time.deltaTime * turn_speed));
+        } else {
+            player_body.Rotate(Vector3.up * 0);
+        }
     }
 }
